0cdc65b0ec11fddadf28441a7ac966959d35c1fe
박지연 <163213733+j02on@users.noreply.github.com>
2024-12-09 09:53:09 +0900

    feat :: setting
    theme.ts : color-system을 세팅하였습니다.

--- src/style/theme.ts
+++ src/style/theme.ts
@@ -1,1 +1,23 @@
-// color-system
+export const theme = {
+  primary: {
+    500: "#1A038E",
+    400: "#2B0FB8",
+    300: "##5133E6",
+    200: "##9985FF",
+    100: "##EAE6FF",
+  },
+  interactive: {
+    error: "#E63338",
+    success: "##44E633",
+    info: "##FAF450",
+    black: "##1F1E21",
+    white: "##FFFFFF",
+  },
+  gray: {
+    500: "#212121",
+    400: "#616161",
+    300: "#9E9E9E",
+    200: "#EEEEEE",
+    100: "#FAFAFA",
+  },
+};
